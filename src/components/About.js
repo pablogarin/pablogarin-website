dd1bffe38a25a3ce5272cb45f727e8227e432143
pablogarin <pablo.garin@hotmail.com>
2021-02-20 10:58:55 -0300

    Feat: created REAL About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,69 +1,16 @@
 import {
   Grid,
-  Typography,
-  Button,
-  Box
-} from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles';
+  Typography
+} from '@material-ui/core';
 
-import DescriptionIcon from '@material-ui/icons/Description';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: '100vh',
-    marginBottom: -56,
-    position: 'relative',
-    zIndex: 1001,
-  },
-  downloadButton: {
-    color: theme.palette.secondary.light,
-    borderColor: theme.palette.secondary.light,
-    borderWidth: 2
-  }
-}))
-
-const About = (props) => {
-  const { isMobile } = props;
-  const classes = useStyles();
-  const downloadResume = () => {
-    window.open('https://docs.google.com/document/d/1GhlNMhgt4ANRdmh9V5WOWPXP0y2LFrbILE3GlD-TfAU/edit?usp=sharing')
-  }
+const About = () => {
   return (
     <Grid
       item
-      container
-      alignItems="center"
-      justify="center"
-      className={classes.root}>
-        <Grid
-          item
-          xs={10}>
-          <Typography
-            variant={isMobile ? 'h2' : 'h1'}
-            component="h1"
-            align="center">
-              Hi! I'm <Typography variant="h1" component="span" color="secondary" style={{fontWeight: 500}}>Pablo Garin!</Typography>
-          </Typography>
-          <Typography
-            variant={isMobile ? 'h4' : 'h3'}
-            component="div"
-            align="center"
-            color="primary">
-              I'm a Full-Stack Web Developer.
-          </Typography>
-          <Box
-            align="center"
-            padding={5}>
-            <Button
-              startIcon={<DescriptionIcon />}
-              variant="outlined"
-              size={isMobile ? 'medium' : 'large'}
-              onClick={() => downloadResume()}
-              className={classes.downloadButton}>
-                Download Resume
-            </Button>
-          </Box>
-        </Grid>
+      container>
+        <Typography>
+          Abot Section
+        </Typography>
     </Grid>
   )
 }
